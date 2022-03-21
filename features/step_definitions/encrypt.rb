88e67cb2e2e75af48278b02b3961e4dc4b1cb0f8
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2022-03-21 13:31:04 +0100

    Add cucumber features

---
+++ features/step_definitions/encrypt.rb
@@ -0,0 +1,15 @@
+Given 'I have {string}' do |data|
+  @data = data
+end
+
+When 'I encrypt the data' do
+  @encrypted_data = Base64.strict_encode64(Asherah.encrypt('partition', @data))
+end
+
+Then 'I should get encrypted_data' do
+  File.write(File.join(TMP_DIR, FILE_NAME), @encrypted_data)
+end
+
+Then 'encrypted_data should not be equal to data' do
+  expect(@encrypted_data).not_to eq(@data)
+end
