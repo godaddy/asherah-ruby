b4806448b941a0691c9a9e8c86ef189a4c5fca17
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2022-03-24 20:47:16 +0100

    Refactor NativeFile

--- ext/asherah/extconf.rb
+++ ext/asherah/extconf.rb
@@ -3,10 +3,5 @@
 require 'mkmf'
 create_makefile('asherah/asherah')
 
-require_relative 'native_downloader'
-require 'cobhan'
-
-root_dir = File.expand_path('../../', __dir__)
-file_name = Class.new.extend(Cobhan).library_file_name('libasherah')
-
-NativeDownloader.download(root_dir, file_name)
+require_relative 'native_file'
+NativeFile.download
--- ext/asherah/native_file.rb
+++ ext/asherah/native_file.rb
@@ -4,9 +4,12 @@ require 'open-uri'
 require 'fileutils'
 require 'digest'
 require 'yaml'
+require 'cobhan'
 
 # Downloads native file and verifies checksum
-class NativeDownloader
+class NativeFile
+  LIB_NAME = 'libasherah'
+  ROOT_DIR = File.expand_path('../../', __dir__)
   CHECKSUMS_FILE = File.expand_path('checksums.yml', __dir__)
   CHECKSUMS = YAML.load_file(CHECKSUMS_FILE)
   VERSION = CHECKSUMS.fetch('version')
@@ -14,8 +17,9 @@ class NativeDownloader
   RETRY_DELAY = 1
 
   class << self
-    def download(root_dir, file_name)
-      lib_dir = File.join(root_dir, 'lib/asherah')
+    def download
+      file_name = Class.new.extend(Cobhan).library_file_name(LIB_NAME)
+      lib_dir = File.join(ROOT_DIR, 'lib/asherah')
       abort "#{lib_dir} does not exist" unless File.exist?(lib_dir)
 
       native_dir = "#{lib_dir}/native"
@@ -24,25 +28,28 @@ class NativeDownloader
       file_path = File.join(native_dir, file_name)
       abort "#{file_path} already exists" if File.exist?(file_path)
 
-      download_file(file_path, file_name)
+      checksum = CHECKSUMS.fetch(file_name) do
+        abort "Unsupported platform - OS: #{FFI::Platform::OS}, ARCH: #{FFI::Platform::ARCH}"
+      end
+
+      content = download_content(file_name)
+
+      sha256 = Digest::SHA256.hexdigest(content)
+      abort "Could not verify checksum of #{file_name}" if sha256 != checksum
+
+      File.binwrite(file_path, content)
     end
 
     private
 
-    def download_file(file_path, file_name)
-      checksum = CHECKSUMS.fetch(file_name) { abort "Unsupported platform for #{file_name}" }
+    def download_content(file_name)
       tries = 0
 
       begin
         tries += 1
         url = "https://github.com/godaddy/asherah-cobhan/releases/download/#{VERSION}/#{file_name}"
         puts "Downloading #{url}"
-        content = URI.parse(url).open.read
-
-        sha256 = Digest::SHA256.hexdigest(content)
-        abort "Could not verify checksum of #{file_name}" if sha256 != checksum
-
-        File.binwrite(file_path, content)
+        URI.parse(url).open.read
       rescue Net::OpenTimeout, Net::ReadTimeout => e
         if tries <= RETRIES
           puts "Got #{e.class}... retrying in #{RETRY_DELAY} seconds"
